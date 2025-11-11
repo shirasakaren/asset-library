fdbcaeb3e1f088b39666b2a6fc1b72491e60ddb5
shirasakaren <ren@shirasaka.ren>
2025-11-11 20:26:44 +0700

    chore(auth): polish keycloak auth guard

--- backend/src/infra/keycloak/keycloak-auth.guard.ts
+++ backend/src/infra/keycloak/keycloak-auth.guard.ts
@@ -37,3 +37,8 @@ export class KeycloakAuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
