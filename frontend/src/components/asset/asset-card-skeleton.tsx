ca71e3c29fc8f0d9983158d5dba8560fa2d3179f
shirasakaren <ren@shirasaka.ren>
2025-11-21 00:29:04 +0700

    chore(ui): polish asset card skeleton

--- frontend/src/components/asset/asset-card-skeleton.tsx
+++ frontend/src/components/asset/asset-card-skeleton.tsx
@@ -40,9 +40,3 @@ export function AssetCardSkeleton({ variant = 'grid', className }: AssetCardSkel
           <Skeleton className="h-3 w-24 mb-2" />
           <Skeleton className="h-5 w-2/3" />
           <Skeleton className="h-3 w-3/4 mt-2" />
-          <Skeleton className="h-3 w-1/2 mt-2" />
-        </div>
-        <Skeleton className="h-9 w-24" />
-      </div>
-    );
-  }
