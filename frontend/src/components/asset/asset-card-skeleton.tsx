1a209f12b277bd838501dc3fb9c4d82f2834405d
shirasakaren <ren@shirasaka.ren>
2026-01-28 12:14:16 +0700

    feat(ui): introduce asset card skeleton and 1 related file

--- frontend/src/components/asset/asset-card-skeleton.tsx
+++ frontend/src/components/asset/asset-card-skeleton.tsx
@@ -42,3 +42,6 @@ export function AssetCardSkeleton({ variant = 'grid', className }: AssetCardSkel
           <Skeleton className="h-3 w-3/4 mt-2" />
           <Skeleton className="h-3 w-1/2 mt-2" />
         </div>
+        <Skeleton className="h-9 w-24" />
+      </div>
+    );
