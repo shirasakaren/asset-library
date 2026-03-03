b0d415553d9503ec6b81034d60dcd04e195534cf
shirasakaren <ren@shirasaka.ren>
2026-03-03 21:27:06 +0700

    refactor(ui): iterate on dropdown menu

--- frontend/src/components/ui/dropdown-menu.tsx
+++ frontend/src/components/ui/dropdown-menu.tsx
@@ -50,7 +50,3 @@ export const DropdownMenuSeparator = forwardRef<
   React.ElementRef<typeof DropdownPrimitive.Separator>,
   React.ComponentPropsWithoutRef<typeof DropdownPrimitive.Separator>
 >(({ className, ...props }, ref) => (
-  <DropdownPrimitive.Separator
-    ref={ref}
-    className={cn('my-1.5 h-px bg-line', className)}
-    {...props}
