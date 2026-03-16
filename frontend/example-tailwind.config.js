028da2677f20df9c2a14b03ce2ba6fe2f5a029de
shirasakaren <ren@shirasaka.ren>
2026-03-16 22:09:12 +0700

    fix(fe): tighten example tailwind config

--- frontend/example-tailwind.config.js
+++ frontend/example-tailwind.config.js
@@ -49,3 +49,6 @@ module.exports = {
         lg: "20px",
         xl: "28px",
       },
+      boxShadow: {
+        "1": "0 1px 2px rgba(14,17,22,0.04), 0 1px 1px rgba(14,17,22,0.03)",
+        "2": "0 6px 24px -8px rgba(14,17,22,0.10), 0 2px 6px -2px rgba(14,17,22,0.05)",
